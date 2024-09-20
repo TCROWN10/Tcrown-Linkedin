8e5eecca4426c79baf7c4013f2287de3d66ed553
Bolaji oluwatobi <bolajioluwatobi249@gmail.com>
2024-09-20 17:37:19 +0100

    Linkedin app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Header from './Component/Header/header';
+import Home from './Component/Home/home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TCROWN LINKEDIN</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
